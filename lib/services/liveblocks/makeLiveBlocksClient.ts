a228abe423b780cc4c180d0c63f087281d5251bb
arcadialogexe-ui <arcadialog.exe@gmail.com>
2026-06-20 22:56:56 -0300

    Update makeLiveBlocksClient.ts

--- lib/services/liveblocks/makeLiveBlocksClient.ts
+++ lib/services/liveblocks/makeLiveBlocksClient.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@liveblocks/client";
 
-export const LIVEBLOCKS_DEV_KEY =
-  "pk_dev_idze__Foj9T2QRR01oBR1su_4y-gl_mx8JwL90_FjQrsrtqxLnvJw5Q1GooxboHQ";
-export const LIVEBLOCKS_PROD_KEY =
-  "pk_prod_DW7rLdIMrMduCwv9ETNWBueq2ih0mUFHJdeisqaw15jZtJrLb6Z_G80IjQS_kBW8";
-export const LIVEBLOCKS_PUBLIC_KEY = LIVEBLOCKS_PROD_KEY;
+export const LIVEBLOCKS_PUBLIC_KEY = "pk_dev_WF18YLyHC6vGKdIbi5y0aCPF3jnLh_2Ef-vdt81JjENNu57ZxbG3Nw3FKETEfrGM";
 
 export function makeLiveBlocksClient() {
   return createClient({
